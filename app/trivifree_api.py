ee9694f1d445d5efea8b76b2f8f7b891ac46048a
AlbertoPuig <alberto.puig.rguez@gmail.com>
2017-10-30 22:04:15 +0100

    api, tests, console_client and environ properties

--- app/trivifree_api.py
+++ app/trivifree_api.py
@@ -17,27 +17,17 @@ class Quest(Resource):
         logging.debug('Init')
         #encoded
         data_string = json.dumps(result)
-
         #Decoded
         decoded = json.loads(data_string)
-
-        #print "Tenemos "+str(decoded["Fruteria"][1]["Verdura"][0]["Cantidad"])+" Lechugas."
-
         #return 'Employees_Name'
         if question_id.isdigit():
-            #return make_response(str(decoded['Trivial'][0]),200)
             question = 'Q'
             question += question_id
             try:
                 with open('data.json', 'r') as f:
                     data = json.load(f)
-                #return all Q1 attributes
-                #return make_response(jsonify(data['Q1']),200)                  
-                #return make_response(jsonify(data['Q1']['CRT']),200)                  
-                #return make_response(jsonify(data[question]['CRT']),200)
                     try:
                         question_data = data[question]
-                        #return make_response(jsonify(data[question]),200)   
                         logging.debug('Response for params: ' + question_id)
                         return make_response(jsonify(question_data),200)       
                     except Exception as ex:
@@ -52,14 +42,8 @@ class Quest(Resource):
             return abort(400)
 
 
-
-
-
-'''api.add_resource(Quest, '/trivifree/api/v1/employees') # Route_1
-api.add_resource(Tracks, '/trivifree/api/v1/tracks') # Route_2'''
 api.add_resource(Quest, '/trivifree/api/v1/quest/<question_id>') # Route_3
 
 
 if __name__ == '__main__':
-     #app.run(port='8080')
-     app.run(host='127.0.0.1', port=8080)
+       app.run(host='127.0.0.1', port=8080)
--- client/client.py
+++ client/client.py
@@ -2,22 +2,26 @@ import sys
 import requests
 from requests.auth import HTTPDigestAuth
 import json
+from ConfigParser import SafeConfigParser
 
 
-Opts = 'Options'
-url = "http://127.0.0.1:5000/trivifree/api/v1/quest/"
-'''qid = 1'''
+def load_properties(env, pproperty):
+	try:
+		parser = SafeConfigParser()
+		parser.read('../conf/environ.properties')
+		vproperty = parser.get(str(env), str(pproperty))
+		return vproperty
+	except Exception as ex_prop:
+		print "Error______: " + str(ex_prop)
+
 
 def get_question(question_id):
 
 	try:
+		url = load_properties('local','url')
 		response = requests.get(url + str(question_id))
 		if response.status_code == 200:
 			jData = response.json()
-			'''print "_________________"
-			print jData['OP1']
-			print "_________________"'''
-			'''return jData['OP1']'''
 			return jData
 		else:
 			return None
@@ -28,9 +32,8 @@ def get_question(question_id):
 def main():
 	qid = 1
 	answers_ok = 0
+	Opts = load_properties('local','Opts')
 	while True:
-	    '''a = raw_input("Enter a number between 1 and 3 or 0 to exit: ")'''
-	    '''a = raw_input(question + '\n' + Opts + '\n' + get_question(2) + '\n' + Op2 + '\n' + Op3 + '\n')'''
 	    qData = get_question(qid)
 	    if qData is not None:
 	    	a = raw_input(qData['QT'] + '\n' + Opts + '\n' + "1: " + qData['OP1'] + '\n' + "2: " + qData['OP2'] + '\n' + "3: " + qData['OP3'] + '\n')
@@ -38,11 +41,10 @@ def main():
 	    	try:
 	        	number = int(a)
 	        	if number == 0:
-	        		print 'bye'
+	        		print 'Bye'
 	        		break
 	        	else: 
 	        		if (0 < number <= 3):
-						print 'Its a correct answer'
 						correct_answer = qData['CRT']
 						correct_answer = int(correct_answer[-1:])
 						print correct_answer
